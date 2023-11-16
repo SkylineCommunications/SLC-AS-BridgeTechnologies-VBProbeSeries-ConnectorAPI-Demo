d5ed4d3f98d50696a666002e7a6383326ab73262
Simon Vandamme <simon.vandamme@skyline.be>
2023-11-16 09:21:36 +0100

    StreamEthCreateBulk

--- StreamEthCreateBulk_2/StreamEthCreateBulk_2.cs
+++ StreamEthCreateBulk_2/StreamEthCreateBulk_2.cs
@@ -45,18 +45,20 @@ Revision History:
 
 DATE		VERSION		AUTHOR			COMMENTS
 
-16/11/2023	1.0.0.1		SVD, Skyline	Initial version
+01/06/2023	1.0.0.1		SVD, Skyline	Initial version
 ****************************************************************************
 */
 
-namespace VBProbe_InterAppDemo_Streams_Eth_CreateBulk_1
+namespace StreamEthCreateBulk
 {
 	using System;
-	using System.Collections.Generic;
-	using System.Globalization;
-	using System.Text;
+
 	using Skyline.DataMiner.Automation;
-	
+	using Skyline.DataMiner.ConnectorAPI.BridgeTechnologies.VBProbeSeries;
+	using Skyline.DataMiner.ConnectorAPI.BridgeTechnologies.VBProbeSeries.Ethernet;
+	using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
+	using Skyline.DataMiner.Core.InterAppCalls.Common.Shared;
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -68,7 +70,111 @@ namespace VBProbe_InterAppDemo_Streams_Eth_CreateBulk_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-	
+			// Get user input
+			////string elementName = "BT VB Probe Series";
+			string elementName = engine.GetScriptParam("Element Name").Value;
+
+			// Find Element
+			var element = engine.FindElement(elementName);
+			if (element == null)
+			{
+				engine.GenerateInformation($"Could not find element with name '{elementName}'.");
+				return;
+			}
+			else
+			{
+				engine.GenerateInformation($"Found element with name '{elementName}' - elemendID '{element.DmaId}/{element.ElementId}'");
+			}
+
+			// Build InterApp Messages
+			var command = InterAppCallFactory.CreateNew();
+			command.Source = new Source("BT VB Series - InterAppDemo - Streams - Eth - Create Bulk");
+			command.ReturnAddress = new ReturnAddress(element.DmaId, element.ElementId, 9000001);
+
+			for (int i = 1; i <= 10; i++)
+			{
+				var message = new CreateEthStream
+				{
+					StreamData = MakeStreamData(i),
+					Source = new Source("BT VB Series - InterAppDemo - Streams - Eth - Create Bulk"),
+				};
+
+				command.Messages.Add(message);
+			}
+
+			// Process InterApp Messages
+			foreach (var responseMessage in command.Send(Engine.SLNetRaw, element.DmaId, element.ElementId, 9000000, new TimeSpan(0, 0, 10), InterApp.KnownTypes))
+			{
+				if (responseMessage != null)
+				{
+					if (responseMessage is CreateEthStreamResult result)
+					{
+						engine.GenerateInformation(result.Description);
+					}
+					else
+					{
+						engine.GenerateInformation($"{nameof(responseMessage)} is not of expected type '{nameof(CreateEthStreamResult)}'.{Environment.NewLine}{responseMessage}");
+					}
+				}
+				else
+				{
+					engine.GenerateInformation($"{nameof(responseMessage)} is null.");
+				}
+			}
+		}
+
+		public EthStreamData MakeStreamData(int i)
+		{
+			var streamData = new EthStreamData
+			{
+				Name = $"Demo_CreateBulk_{i}",
+				Address = $"239.0.20.1{i}",
+				Port = $"591{i}",
+				VLanId = "0",
+
+				EthThresholds = "Default",
+				EtrThresholds = "Default",
+				ServiceThresholds = "Default",
+				PidThresholds = "Default",
+				ReferenceThresholds = "Default",
+				VbcThresholds = "Default",
+				ContentThresholds = "Default",
+
+				Join = true,
+				JoinInterface = "eth0",
+
+				IsEtrEnabled = false,
+				EtrEngine = "1",
+
+				IsFecEnabled = false,
+				T2miStream = String.Empty,
+				T2miPid = "0",
+				T2miPlpId = "0",
+				Dash7Stream1 = String.Empty,
+				Dash7Stream2 = String.Empty,
+
+				SsmAddress = "0.0.0.1",
+				SsmName = String.Empty,
+				SsmAddress2 = "0.0.0.0",
+				SsmName2 = String.Empty,
+				SsmAddress3 = "0.0.0.0",
+				SsmName3 = String.Empty,
+				SsmAddress4 = "0.0.0.0",
+				SsmName4 = String.Empty,
+				SsmAddress5 = "0.0.0.0",
+				SsmName5 = String.Empty,
+
+				IsSrtEnabled = true,
+				SrtHost = String.Empty,
+				SrtPort = 1,
+				SrtMode = SrtMode.Listener,
+				SrtPassphrase = String.Empty,
+				SrtLatency = 0,
+
+				Page = 1,
+			};
+
+			return streamData;
 		}
 	}
 }
