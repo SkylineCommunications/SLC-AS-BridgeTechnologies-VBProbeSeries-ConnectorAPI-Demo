b2b0563ba120c8df193f283ee3cf0858125f6bb8
Simon Vandamme <simon.vandamme@skyline.be>
2025-02-21 17:18:35 +0100

    Thershold PIDs

---
+++ Alarm Thresholds/PID Thresholds/ThresholdPidCreateBulk/ThresholdPidCreateBulk.cs
@@ -0,0 +1,143 @@
+/*
+****************************************************************************
+*  Copyright (c) 2023,  Skyline Communications NV  All Rights Reserved.    *
+****************************************************************************
+
+By using this script, you expressly agree with the usage terms and
+conditions set out below.
+This script and all related materials are protected by copyrights and
+other intellectual property rights that exclusively belong
+to Skyline Communications.
+
+A user license granted for this script is strictly for personal use only.
+This script may not be used in any way by anyone without the prior
+written consent of Skyline Communications. Any sublicensing of this
+script is forbidden.
+
+Any modifications to this script by the user are only allowed for
+personal use and within the intended purpose of the script,
+and will remain the sole responsibility of the user.
+Skyline Communications will not be responsible for any damages or
+malfunctions whatsoever of the script resulting from a modification
+or adaptation by the user.
+
+The content of this script is confidential information.
+The user hereby agrees to keep this confidential information strictly
+secret and confidential and not to disclose or reveal it, in whole
+or in part, directly or indirectly to any person, entity, organization
+or administration without the prior written consent of
+Skyline Communications.
+
+Any inquiries can be addressed to:
+
+	Skyline Communications NV
+	Ambachtenstraat 33
+	B-8870 Izegem
+	Belgium
+	Tel.	: +32 51 31 35 69
+	Fax.	: +32 51 31 01 29
+	E-mail	: info@skyline.be
+	Web		: www.skyline.be
+	Contact	: Ben Vandenberghe
+
+****************************************************************************
+Revision History:
+
+DATE		VERSION		AUTHOR			COMMENTS
+
+01/06/2023	1.0.0.1		SVD, Skyline	Initial version
+****************************************************************************
+*/
+
+namespace ThresholdPidCreateBulk
+{
+	using System;
+
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.ConnectorAPI.BridgeTechnologies.VBProbeSeries;
+	using Skyline.DataMiner.ConnectorAPI.BridgeTechnologies.VBProbeSeries.AlarmThresholds.PidThresholds;
+	using Skyline.DataMiner.Core.InterAppCalls.Common.CallBulk;
+	using Skyline.DataMiner.Core.InterAppCalls.Common.Shared;
+
+	/// <summary>
+	/// Represents a DataMiner Automation script.
+	/// </summary>
+	public class Script
+	{
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			// Get user input
+			////string elementName = "BT VB Probe Series";
+			string elementName = engine.GetScriptParam("Element Name").Value;
+
+			// Find Element
+			var element = engine.FindElement(elementName);
+			if (element == null)
+			{
+				engine.GenerateInformation($"Could not find element with name '{elementName}'.");
+				return;
+			}
+			else
+			{
+				engine.GenerateInformation($"Found element with name '{elementName}' - elemendID '{element.DmaId}/{element.ElementId}'");
+			}
+
+			// Build InterApp Message
+			var command = InterAppCallFactory.CreateNew();
+			command.Source = new Source("BT VB Series - InterAppDemo - Thresholds - PID - Create Bulk");
+			command.ReturnAddress = new ReturnAddress(element.DmaId, element.ElementId, 9000001);
+
+			for (int i = 1; i <= 10; i++)
+			{
+				var message = new CreatePidThreshold
+				{
+					ThresholdData = MakeThresholdData(i),
+					Source = new Source("BT VB Series - InterAppDemo - Thresholds - PID - Create Bulk"),
+				};
+
+				command.Messages.Add(message);
+			}
+
+			// Process InterApp Message
+			foreach (var responseMessage in command.Send(
+				Engine.SLNetRaw,
+				element.DmaId,
+				element.ElementId,
+				9_000_000,
+				new TimeSpan(0, 0, 10),
+				InterApp.KnownTypes))
+			{
+				if (responseMessage != null)
+				{
+					if (responseMessage is CreatePidThresholdResult result)
+					{
+						engine.GenerateInformation(result.Description);
+					}
+					else
+					{
+						engine.GenerateInformation($"{nameof(responseMessage)} is not of expected type '{nameof(CreatePidThresholdResult)}'.{Environment.NewLine}{responseMessage}");
+					}
+				}
+				else
+				{
+					engine.GenerateInformation($"{nameof(responseMessage)} is null.");
+				}
+			}
+		}
+
+		public PidThresholdData MakeThresholdData(int i)
+		{
+			var thresholdData = new PidThresholdData
+			{
+				Name = $"Demo_CreateBulk_1{i}",
+				Description = $"Demo_CreateBulk_1{i}",
+			};
+
+			return thresholdData;
+		}
+	}
+}
